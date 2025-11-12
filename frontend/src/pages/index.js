7597871c3245d89d28072a5ddbca5424d7dd7b95
FaizanSiddiuqe <154720299+FaizanSiddiuqe@users.noreply.github.com>
2025-11-12 23:18:09 +0500

    correct naming convention error to Error

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -9,14 +9,12 @@ import BooksByCategory from "./book/BooksByCategory";
 import FeaturedBooks from "./book/FeaturedBooks";
 import LatestBooks from "./book/LatestBooks";
 import WishlistPage from "./book/WishlistPage";
-import ErrorPage from "./errorPage";
+import ErrorPage from "./ErrorPage";
 import CheckoutPage from "./order/CheckoutPage";
 import OrderCancellation from "./order/OrderCancellation";
 import OrderConfirmation from "./order/OrderConfirmation";
 import SignUp from "./SignUp";
 import SignIn from "./SingIn";
-
-
 
 export {
   BookDetailPage,
